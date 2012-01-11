9942e858440be2ea51efb201b5329d845984c677
Jelle de Jong <jelle@hoest.nl>
2012-01-11 17:47:22 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+/* Author:
+
+*/
+
+$(function() {
+  $('header').click(function() {
+    location.href = 'http://www.colthoffcommunicatie.nl/';
+  }).css({
+    'cursor': 'pointer'
+  });
+
+  $('.pivotx-download').each(openInNewWindow);
+  $('.external').each(openInNewWindow);
+});
+
+openInNewWindow = function(index, value) {
+  $(value).click(function() {
+    window.open($(this).attr('href'));
+    return false;
+  });
+}
+
+validate = function(formObj) {
+  var valid = true;
+  $(formObj).find('.required').each(function(index, value) {
+    if($(value).val() == '') {
+      valid = false;
+      $(value).css('border-color', '#dd0000');
+    }
+  });
+
+  if(!valid) {
+    alert('U heeft niet alle verplichte velden ingevoerd.');
+  }
+
+  return valid;
+}
